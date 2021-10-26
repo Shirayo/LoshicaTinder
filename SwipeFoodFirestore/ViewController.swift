e289e8625158a07d12bb4d20375363d0088412f0
Shirayo <iphoje@gmail.com>
2021-10-26 17:43:16 +0300

    add vertical stack and top horizontal for future app

--- SwipeFoodFirestore/ViewController.swift
+++ SwipeFoodFirestore/ViewController.swift
@@ -12,8 +12,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello world")
+        
+        let subViews = [UIColor.lightGray, UIColor.gray, UIColor.darkGray].map { color -> UIView in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        let topStackView = UIStackView(arrangedSubviews: subViews)
+        topStackView.distribution = .fillEqually
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+        stackView.distribution = .fill
+        stackView.axis = .vertical
+        
+        view.addSubview(stackView)
+        stackView.fillSuperView()
     }
 
-
 }
+
+
 
