21d536ebbc8a4a8cff342f1bce28cd1c6baf4ed4
Shirayo <iphoje@gmail.com>
2021-11-11 14:54:45 +0300

    Created SettingsCell for SettingsView and added custom fields for tableview

--- LoshicaTinder/Controllers/SettingsController.swift
+++ LoshicaTinder/Controllers/SettingsController.swift
@@ -15,6 +15,12 @@ class CustomImagePcikerController: UIImagePickerController {
 
 class SettingsController: UITableViewController {
    
+    class HeaderLabel: UILabel {
+        override func drawText(in rect: CGRect) {
+            super.drawText(in: rect.insetBy(dx: 16, dy: 0))
+        }
+    }
+    
     func createButton(selector: Selector) -> UIButton {
         let button = UIButton(type: .system)
         button.setTitle("selectPhoto", for: .normal)
@@ -29,6 +35,20 @@ class SettingsController: UITableViewController {
     lazy var image1Button = createButton(selector: #selector(handleSelectPhoto))
     lazy var image2Button = createButton(selector: #selector(handleSelectPhoto))
     lazy var image3Button = createButton(selector: #selector(handleSelectPhoto))
+    lazy var header: UIView = {
+        let header = UIView()
+        let padding: CGFloat = 16
+        header.addSubview(image1Button)
+        image1Button.anchor(top: header.topAnchor, leading: header.leadingAnchor, bottom: header.bottomAnchor, trailing: nil, padding: .init(top: padding, left: padding, bottom: padding, right: 0))
+        image1Button.widthAnchor.constraint(equalTo: header.widthAnchor, multiplier: 0.45).isActive = true
+        let stackView = UIStackView(arrangedSubviews: [image2Button, image3Button])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = padding
+        header.addSubview(stackView)
+        stackView.anchor(top: header.topAnchor, leading: image1Button.trailingAnchor, bottom: header.bottomAnchor, trailing: header.trailingAnchor, padding: .init(top: padding, left: padding, bottom: padding, right: padding))
+        return header
+    }()
 
 
     @objc fileprivate func handleSelectPhoto(button: UIButton) {
@@ -50,25 +70,58 @@ class SettingsController: UITableViewController {
             UIBarButtonItem(title: "logout", style: .done, target: self, action: #selector(handleCancel))
         ]
         tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        tableView.keyboardDismissMode = .interactive
     }
     
+    //MARK: override functions
+    
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = UIView()
-        let padding: CGFloat = 16
-        header.addSubview(image1Button)
-        image1Button.anchor(top: header.topAnchor, leading: header.leadingAnchor, bottom: header.bottomAnchor, trailing: nil, padding: .init(top: padding, left: padding, bottom: padding, right: 0))
-        image1Button.widthAnchor.constraint(equalTo: header.widthAnchor, multiplier: 0.45).isActive = true
-        let stackView = UIStackView(arrangedSubviews: [image2Button, image3Button])
-        stackView.axis = .vertical
-        stackView.distribution = .fillEqually
-        stackView.spacing = padding
-        header.addSubview(stackView)
-        stackView.anchor(top: header.topAnchor, leading: image1Button.trailingAnchor, bottom: header.bottomAnchor, trailing: header.trailingAnchor, padding: .init(top: padding, left: padding, bottom: padding, right: padding))
-        return header
+        if section == 0 {
+            return header
+        }
+        let headerLabel = HeaderLabel()
+        switch section {
+        case 1:
+            headerLabel.text = "Name"
+        case 2:
+            headerLabel.text = "Profession"
+        case 3:
+            headerLabel.text = "Age"
+        case 4:
+            headerLabel.text = "Bio"
+        default:
+            headerLabel.text = "default"
+        }
+        return headerLabel
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? 0 : 1
     }
     
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 300
+        return section == 0 ? 300 : 40
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = SettingsCell(style: .default, reuseIdentifier: nil)
+        switch indexPath.section {
+        case 1:
+            cell.textField.placeholder = "Enter name"
+        case 2:
+            cell.textField.placeholder = "Enter profession"
+        case 3:
+            cell.textField.placeholder = "Enter age"
+        case 4  :
+            cell.textField.placeholder = "Enter bio"
+        default:
+            cell.textField.placeholder = "default placeholder"
+        }
+        return cell
     }
     
     @objc fileprivate func handleCancel() {
---
+++ LoshicaTinder/Views/SettingsCell.swift
@@ -0,0 +1,41 @@
+//
+//  SettingsCell.swift
+//  LoshicaTinder
+//
+//  Created by Vasily Mordus on 11.11.21.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+
+    class SettingsTextField: UITextField {
+        override func textRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override func editingRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override var intrinsicContentSize: CGSize {
+            return .init(width: 0, height: 44)
+        }
+    }
+    
+    let textField: SettingsTextField = {
+        let tf = SettingsTextField()
+        tf.placeholder = "Enter name"
+        return tf
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(textField)
+        textField.fillSuperView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
