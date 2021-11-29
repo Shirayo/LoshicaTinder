1b7c8cf166b9dcda1177783d09f3a123b581152d
Shirayo <iphoje@gmail.com>
2021-11-29 16:17:53 +0300

    Added fetch matches from Firebase Firestore to collection in MatchesController

--- LoshicaTinder/Controllers/MatchesMessages/MatchMessagesController.swift
+++ LoshicaTinder/Controllers/MatchesMessages/MatchMessagesController.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
+
+struct Match {
+    
+    let name: String
+    let profileImageUrl: String
+    
+    init(from dict: [String: Any]) {
+        self.name = dict["name"] as? String ?? ""
+        self.profileImageUrl = dict["imageUrl"] as? String ?? ""
+    }
+}
 
 class MatchCell: UICollectionViewCell {
     
     let profileImageView = UIImageView(image: UIImage(named: "nikita3"))
     let usernameLabel = UILabel(text: "Username Here", font: .systemFont(ofSize: 14), textColor: .black, textAlignment: .center, numberOfLines: 2)
     
+    var matchUserProfile: Match! {
+        didSet {
+            guard let imageUrl = URL(string: matchUserProfile.profileImageUrl) else { return }
+            profileImageView.sd_setImage(with: imageUrl)
+            usernameLabel.text = matchUserProfile.name
+        }
+    }
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         profileImageView.contentMode = .scaleAspectFill
@@ -35,36 +56,56 @@ class MatchCell: UICollectionViewCell {
 class MatchMessagesController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     let customNavBar = MatchesTopNavigationBar()
+    var users = [Match]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+        fetchMatches()
+    }
+    
+    fileprivate func fetchMatches() {
+        guard let currentUser = Auth.auth().currentUser?.uid else { return }
+        Firestore.firestore().collection("matches_messages").document(currentUser).collection("matches").getDocuments { querySnapshot, error in
+            if let err = error {
+                print(err)
+                return
+            }
+            querySnapshot?.documents.forEach({ documentSnapshot in
+                let dictionary = documentSnapshot.data()
+                self.users.append(Match(from: dictionary))
+                print(documentSnapshot.data())
+            })
+            self.collectionView.reloadData()
+        }
+    }
+    
+    fileprivate func setupLayout() {
+        collectionView.backgroundColor = .white
+        collectionView.register(MatchCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.contentInset.top = 140
+        view.addSubview(customNavBar)
+        customNavBar.anchor(top: self.view.safeAreaLayoutGuide.topAnchor, leading: self.view.leadingAnchor, bottom: nil, trailing: self.view.trailingAnchor, padding: .zero, size: .init(width: 0, height: 140))
+        customNavBar.backButton.addTarget(self, action: #selector(handleBack), for: .touchUpInside)
+    }
+    
+    //MARK: datasource/delegate functions
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return items.count
+        return users.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MatchCell
-        print(indexPath.row)
-        
-        cell.backgroundColor = colors[indexPath.row]
+        cell.matchUserProfile = users[indexPath.row]
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return .init(width: 80, height: 120)
     }
-    
-        
-    var items = ["TEST1", "TEST2", "TEST3", "TEST4",]
-    var colors: [UIColor] = [UIColor.red, .blue, .green, .orange]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        collectionView.backgroundColor = .white
-        collectionView.register(MatchCell.self, forCellWithReuseIdentifier: "cell")
-        collectionView.contentInset.top = 150
-        view.addSubview(customNavBar)
-        customNavBar.anchor(top: self.view.safeAreaLayoutGuide.topAnchor, leading: self.view.leadingAnchor, bottom: nil, trailing: self.view.trailingAnchor, padding: .zero, size: .init(width: 0, height: 140))
-        customNavBar.backButton.addTarget(self, action: #selector(handleBack), for: .touchUpInside)
-
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 16, left: 16, bottom: 16, right: 16)
     }
 
     @objc fileprivate func handleBack() {
